ca3ef37ebe60594c48d8632d8c9be92ac9533a68
ssanidas <sotiris.sanidas@gmail.com>
2015-07-24 19:11:13 +0200

    added predictions directory in the bacckup script

--- backup.sh
+++ backup.sh
@@ -6,3 +6,6 @@ backupdir="/home/sanidas/backup"
 
 #rsyncing the TarLog
 rsync -ah --delete /projects/0/lotaas/data/out/TarLog $backupdir
+
+#rsyncing the predictions
+rsync -ah --delete /projects/0/lotaas/data/predictions $backupdir
